38eff9a57b675276a02f1dff09e725d6b863a42f
mallebo <78452531+mallebo@users.noreply.github.com>
2021-06-16 20:24:11 +0000

    nun

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 // Author:
 
 // Global UI Variables
+let canvasDiv;
+let canvas;
+let textDiv;
+let textP;
+let buttonDiv;
+let resetButton;
+let snake;
+let food;
+let resolution;
+let scaledWidth;
+let scaledHeight;
+let score;
 //let canvasDiv;
 
 // Global Game Variables
@@ -8,18 +20,36 @@
 
 function setup() {
   // Build the interface
-
+canvasDiv = createDiv();
+canvas = createCanvas(640, 480);
+canvas.parent(canvasDiv);
   // Set the resolution to 20. Play with this later if you want.
+textDiv = createDiv();
+textP = createP();
+textP.parent(textDiv);
 
   // Scaled width and height are width / resolution, height / resolution
-
+buttonDiv = createDiv();
+resetButton = createButton("Reset Game");
+resetButton.mousePressed(resetGame);
+resetButton.parent(buttonDiv);
   // Set the game's framerate to 5 (or whatever you prefer)
+resolution = 20;
+scaledWidth = floor(width / resolution);
+scaledHeight = floor(height / resolution);
+frameRate(5);
+resetGame();
+
+
 
   // Call resetGame() to initialize everything else.
 
 }
 
 function draw() {
+
+  snake.update();
+  snake.show();
   // Scale the canvas according to resolution, then refresh the background
 
   // Check if snake is eating the food
@@ -41,5 +71,8 @@ function keyPressed() {
 }
 
 function resetGame() {
+   snake = new Snake();
+
+
 
 }
--- snake.js
+++ snake.js
@@ -1,14 +1,33 @@
 class Snake {
 
   constructor() {
+    this.body = [];
+    this.body[0] = createVector(floor(scaledWidth / 2),  floor(scaledHeight / 2));
+    this.xDirection = 0;
+    this.yDirection = 0;
+
+    
 
   }
 
   update() {
+    let head = this.body[this.body.length - 1].copy();
+    this.body.shift();
+    head.x += this.xDirection;
+    head.y += this.yDirection;
+    this.body.push(head);
+
+
+
+
+
+
+
 
   }
 
   endGame() {
+    
 
   }
 
@@ -21,10 +40,23 @@ class Snake {
   }
 
   show() {
+    for(let i = 0; i < this.body.length; i++) {
+      fill(0);
+      noStroke();
+      rect(this.body[i].x, this.body[i].y, 1, 1);
+
+}
+
+
+
+
+}
 
   }
 
   setDirection(x, y) {
+    this.xDirection = x;
+    this.yDirection = y;
 
   }
 
